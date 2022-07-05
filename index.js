038d94fc09e30b7dc71afe8c37e6afaf1a13bfff
Freakidann <99401816+Freakidann@users.noreply.github.com>
2022-07-05 23:45:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("hello");
+	bot.chat("/register bot1234");
 });
 
 bot.on('time', function(time) {
 	if(nightskip == "true"){
 	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/time set day')
+	bot.chat('/register bot1234')
 	}}
     if (connected <1) {
         return;
